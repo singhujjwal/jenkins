2df5226efedae1495be55ea2347a1795983295b8
Fox <singhujjwal@gmail.com>
2021-10-20 17:14:01 +0530

    Update Utilities.groovy

--- lib/src/org/foo/Utilities.groovy
+++ lib/src/org/foo/Utilities.groovy
@@ -3,6 +3,7 @@ class Utilities implements Serializable {
   def steps
   Utilities(steps) {this.steps = steps}
   def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/usr/bin/mvn -o ${args}"
+    // steps.sh "${steps.tool 'Maven'}/usr/bin/mvn -o ${args}"
+    steps.sh "${steps.tool 'Echo'}echo -o ${args}"
   }
 }
