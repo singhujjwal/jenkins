f6a4ad5457462201f44fe2d1434d70d7eea523f2
Singh Ujjwal <singhujjwal@gmail.com>
2021-12-23 17:02:10 +0530

    .

--- pythonutils/arg_verifier.py
+++ pythonutils/arg_verifier.py
@@ -1,5 +1,25 @@
 
 
+import argparse
+
+
+def parse_command_line():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-a", "--account",
+                        default=None, required=True,
+                        help=("Target AWS region"))
+    parser.add_argument("-r", "--region",
+                        default=None, required=True,
+                        help=("Target AWS region"))
+    parser.add_argument("-n", "--dry-run", action='store_true',
+                        default=False, required=False,
+                        help=("Print out the images but don't delete it "))
+    parser.add_argument("-t", "--title",
+                        default=None, required=False,
+                        help=("Inspector vulnerability title to search for"))
+    args = parser.parse_args()
+    return args
 
 def main():
     print("This is my testing file")
