24451dd04f1569ee5979820d7a03f49a3884edc5
Singh Ujjwal <singhujjwal@gmail.com>
2021-12-28 11:20:02 +0530

    Scripted pipeline

--- lib/vars/AllAccounts.groovy
+++ lib/vars/AllAccounts.groovy
@@ -3,9 +3,6 @@ import org.foo.AzureAccountcls
 
 
 List<String> getAllAccountsList() {
-
-    final List<String> allAccountList = []
-    allAccountList.add(AwsAccountcls.values())
-    allAccountList.add(AzureAccountcls.values())
+    return AwsAccountcls.values().collect { it.name() } + AzureAccountcls.values().collect { it.name() }
     return allAccountList
 }
