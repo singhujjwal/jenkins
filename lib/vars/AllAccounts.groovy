51578fb94fdcad9cd3e63b0908e53a486892d132
Fox <singhujjwal@gmail.com>
2022-02-02 09:40:21 +0530

    Only functions in var can be used

--- lib/vars/AllAccounts.groovy
+++ lib/vars/AllAccounts.groovy
@@ -4,5 +4,4 @@ import org.foo.AzureAccountcls
 
 List<String> getAllAccountsList() {
     return AwsAccountcls.values().collect { it.name() } + AzureAccountcls.values().collect { it.name() }
-    return allAccountList
 }
